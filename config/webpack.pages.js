05ba09185d0746c8c8e7e5e856cd157de96c32f4
Polintics <pvleukhina@edu.hse.ru>
2025-10-09 15:57:15 +0900

    init commit3

--- config/webpack.pages.js
+++ config/webpack.pages.js
@@ -1,22 +1,27 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 function createPage(template, filename) {
   return new HtmlWebpackPlugin({
     template: template,
-    filename: filename
-  })
+    filename: filename,
+  });
 }
 
 const htmlPages = [
   createPage('./src/index.html', './index.html'),
   createPage('./src/pages/articles.html', './pages/articles.html'),
-  createPage('./src/pages/dictionary.html', './pages/dictionary.html'),
-  createPage('./src/pages/tests.html', './pages/tests.html'),
-  createPage('./src/pages/tests/test1.html', './pages/tests/test1.html'),
   createPage(
     './src/pages/articles/article1.html',
-    './pages/articles/article1.html'
-  )
-]
+    './pages/articles/article1.html',
+    './src/pages/articles/article2.html',
+    './pages/articles/article2.html',
+    './src/pages/articles/article3.html',
+    './pages/articles/article3.html',
+    './src/pages/articles/article4.html',
+    './pages/articles/article4.html'
+  ),
+  createPage('./src/pages/tarelka.html', './pages/tarelka.html'),
+  createPage('./src/pages/search.html', './pages/search.html'),
+];
 
-module.exports = htmlPages
+module.exports = htmlPages;
