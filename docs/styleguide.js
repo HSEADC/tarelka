9473971e01564c2fab50a474a5f81d8241a8ae63
Polintics <pvleukhina@edu.hse.ru>
2025-12-26 16:09:54 +0900

    стайлгайд с адаптивом, главная о нас и статьи десктоп

---
+++ docs/styleguide.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";document.addEventListener("DOMContentLoaded",function(){document.getElementById("burger").addEventListener("click",function(){document.querySelector("header").classList.toggle("open")})})})();
