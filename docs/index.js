fc2a3ad9c9c029f9b1f499684973a6ee915b3078
Lidiia Pimenova <Lidapmnv@gmail.com>
2026-05-03 12:01:22 +0300

    add images for og

--- docs/index.js
+++ docs/index.js
@@ -1,1 +1,18 @@
 (()=>{"use strict";console.log("main")})();
+
+document.addEventListener('DOMContentLoaded', function () {
+  const burger = document.getElementById('burger');
+  const header = document.querySelector('.header');
+  if (burger && header) {
+    burger.addEventListener('click', function () {
+      header.classList.toggle('open');
+    });
+
+    const menuLinks = document.querySelectorAll('.menu_link');
+    menuLinks.forEach(link => {
+      link.addEventListener('click', function () {
+        header.classList.remove('open');
+      });
+    });
+  }
+});
